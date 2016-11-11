9ef065255a3f9f8db01eee5133c69421eea4f7a4
Paige Fox <paige.vict.fox@gmail.com>
2016-11-10 22:26:58 -0800

    poop

--- .profile.d/setpath.sh
+++
@@ -1,4 +0,0 @@
-PROFILE_PATH="$BUILD_DIR/.profile.d/mecab.sh"
-mkdir -p $(dirname $PROFILE_PATH)
-echo 'export MECAB_PATH=/app/lib/libmecab.so' >> $PROFILE_PATH
-echo 'export MECAB_CHARSET=euc-jp' >> $PROFILE_PATH
